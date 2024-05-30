5204e09d7f28cd8e21b97a4779a2208ad017285e
chmsly <chris.c.hemsley@gmail.com>
2024-05-30 15:19:47 -0600

    SecurityConfig and Db

---
+++ DentalAI/src/main/java/database/Db.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDynamoDBRepositories
+public class DynamoDBConfig {
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
+        amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        return amazonDynamoDB;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    }
+}
