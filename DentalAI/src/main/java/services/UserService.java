d3902ba5b662266ba00025fcda9b44ca28e1ee4a
chmsly <chris.c.hemsley@gmail.com>
2024-06-14 09:46:16 -0700

    initialize UserService

---
+++ DentalAI/src/main/java/services/UserService.java
@@ -0,0 +1,20 @@
+package com.example.dentalxray.service;
+
+import com.example.dentalxray.model.User;
+import com.example.dentalxray.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findById(username).orElse(null);
+    }
+}
