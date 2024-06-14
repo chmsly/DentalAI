431d8377e882426147c45a6f21132a66981f6e61
chmsly <chris.c.hemsley@gmail.com>
2024-06-14 11:04:37 -0700

    xray model

---
+++ DentalAI/src/main/java/Models/XrayImage.java
@@ -0,0 +1,39 @@
+package com.example.dentalxray.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "XrayImage")
+public class XrayImage {
+    private String id;
+    private String userId;
+    private String s3Url;
+
+    @DynamoDBHashKey(attributeName = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "s3Url")
+    public String getS3Url() {
+        return s3Url;
+    }
+
+    public void setS3Url(String s3Url) {
+        this.s3Url = s3Url;
+    }
+}
