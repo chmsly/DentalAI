9ae6c03ca8ff22765ddebd37eb057f37ff245efe
chmsly <chris.c.hemsley@gmail.com>
2024-06-14 10:58:25 -0700

    auth controller

--- DentalAI/src/main/java/controllers/AuthController.java
+++ DentalAI/src/main/java/controllers/AuthController.java
@@ -1,4 +1,31 @@
-package controllers;
+package com.example.dentalxray.controller;
 
+import com.example.dentalxray.model.User;
+import com.example.dentalxray.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        User existingUser = userService.findByUsername(user.getUsername());
+        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+            // Authentication logic here
+            return ResponseEntity.ok("User logged in successfully");
+        }
+        return ResponseEntity.status(401).body("Invalid credentials");
+    }
 }
