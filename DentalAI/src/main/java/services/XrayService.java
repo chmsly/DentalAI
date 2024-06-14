42345373008661e47962693f1278c84dffb0bb63
chmsly <chris.c.hemsley@gmail.com>
2024-06-14 10:54:55 -0700

    Xray service work

---
+++ DentalAI/src/main/java/services/XrayService.java
@@ -0,0 +1,34 @@
+package com.example.dentalxray.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.dentalxray.model.XrayImage;
+import com.example.dentalxray.repository.XrayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class XrayService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    private XrayRepository xrayRepository;
+
+    public String uploadXray(MultipartFile file, String userId) throws IOException {
+        String bucketName = "your-bucket-name";
+        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), new ObjectMetadata()));
+        String s3Url = amazonS3.getUrl(bucketName, fileName).toString();
+
+
+
+        return s3Url;
+    }
+}
