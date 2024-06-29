6e079cd38c7a32231687d910cf572f4d0866d750
chmsly <chris.c.hemsley@gmail.com>
2024-06-28 22:08:56 -0600

    ExceptionHandler

---
+++ DentalAI/src/main/java/controllers/ExceptionHandlerController.java
@@ -0,0 +1,16 @@
+package com.example.dentalxray.controller;
+
+import com.example.dentalxray.exception.CustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
