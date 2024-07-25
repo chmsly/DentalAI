bdc845b66c72246c19330abae0b9459fe084c473
chmsly <chris.c.hemsley@gmail.com>
2024-07-24 19:01:43 -0600

    login.js changes

---
+++ DentalAI/src/frontend/login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {username: '', password: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Handle form submission logic here
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" onChange={this.handleChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default Login;
