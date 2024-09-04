bcf00574e524deb3d8f77b0f905fcb66765ae13d
chmsly <chris.c.hemsley@gmail.com>
2024-09-04 16:24:23 -0600

    XrayAnalysisService work

--- DentalAI/src/main/java/services/XrayService.java
+++ DentalAI/src/main/java/services/XrayService.java
@@ -1,43 +1,50 @@
 package com.example.dentalxray.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.example.dentalxray.model.XrayImage;
-import com.example.dentalxray.repository.XrayRepository;
+import com.example.dentalxray.model.*;
+import com.example.dentalxray.repository.AnalysisReportRepository;
+import com.example.dentalxray.exception.AnalysisException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
-public class XrayService {
+public class AIAnalysisService {
 
     @Autowired
-    private AmazonS3 amazonS3;
+    private AIModelService aiModelService;
 
     @Autowired
-    private XrayRepository xrayRepository;
+    private AnalysisReportRepository analysisReportRepository;
 
-    public String uploadXray(MultipartFile file, String userId) throws IOException {
-        String bucketName = "your-bucket-name";
-        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
-        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), new ObjectMetadata()));
-        String s3Url = amazonS3.getUrl(bucketName, fileName).toString();
-
-
-        return s3Url;
+    public AnalysisResult analyzeXray(MultipartFile xrayImage, String dentistDiagnosis, String userId) throws AnalysisException {
+        try {
+            BufferedImage processedImage = preprocessImage(xrayImage);
+            AIModelResult aiResult = runAIModel(processedImage);
+            ComparisonResult comparison = compareResults(aiResult, dentistDiagnosis);
+            AnalysisReport report = generateReport(comparison, userId, xrayImage.getOriginalFilename());
+            saveAnalysisResult(report);
+            return new AnalysisResult(report, generateHeatmap(processedImage, aiResult));
+        } catch (IOException e) {
+            throw new AnalysisException("Error processing X-ray image", e);
+        } catch (Exception e) {
+            throw new AnalysisException("Error during X-ray analysis", e);
+        }
     }
-    public void saveXray(XrayImage xrayImage) {
-        xrayRepository.save(xrayImage);
-    }
-    public XrayImage findById(String id) {
-        return xrayRepository.findById(id).orElse(null);
-    }
 
-    public XrayImage findByUserId(String userId) {
-        return xrayRepository.findByUserId(userId);
+    // ... (other methods remain the same)
+
+    private void saveAnalysisResult(AnalysisReport report) throws AnalysisException {
+        try {
+            analysisReportRepository.save(report);
+        } catch (Exception e) {
+            throw new AnalysisException("Error saving analysis report", e);
+        }
     }
 }
