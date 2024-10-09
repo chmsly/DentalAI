28ac82a1c4fc38eae43015e8ad85c21750ace52d
chmsly <chris.c.hemsley@gmail.com>
2024-10-09 16:55:05 -0600

    User work

--- DentalAI/src/main/java/models/User.java
+++ DentalAI/src/main/java/models/User.java
@@ -1,55 +1,34 @@
-package com.example.dentalxray.model;
+package com.dentalai.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @NotBlank
-    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String username;
-
-    @NotBlank
     private String password;
-
-    @NotBlank
-    private String role;
-
-    public Long getId() {
+    public long getId() {
         return id;
     }
-
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 
     
