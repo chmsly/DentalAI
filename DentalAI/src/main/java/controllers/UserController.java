b4ac0a3f33991e69304d67bc259f6c4b7f8baa96
chmsly <chris.c.hemsley@gmail.com>
2024-10-17 20:18:28 -0600

    Update controllers to use global exception handler

--- DentalAI/src/main/java/controllers/UserController.java
+++ DentalAI/src/main/java/controllers/UserController.java
@@ -1,10 +1,12 @@
+package com.dentalai.controller;
+
+import com.dentalai.model.User;
+import com.dentalai.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,57 +16,20 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody User user) {
-        try {
-            if (userService.findByUsername(user.getUsername()) != null) {
-                throw new CustomException("Username is already taken");
-            }
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            User savedUser = userService.saveUser(user);
-            return ResponseEntity.ok(savedUser);
-        } catch (CustomException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new CustomException("Error registering user: " + e.getMessage());
-        }
+    public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            String jwt = jwtTokenProvider.generateToken(authentication);
-            Map<String, String> response = new HashMap<>();
-            response.put("token", jwt);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            throw new CustomException("Error logging in: " + e.getMessage());
-        }
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<?> logoutUser() {
-        try {
-            SecurityContextHolder.clearContext();
-            return ResponseEntity.ok("User logged out successfully");
-        } catch (Exception e) {
-            throw new CustomException("Error logging out: " + e.getMessage());
-        }
-    }
-
-    @GetMapping("/user/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
 
-    @DeleteMapping("/user/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
-        User user = userService.findById(id);
-        userService.deleteUser(user);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
 }
